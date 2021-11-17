f7c465ac956984bf29f54e5e3a158942cd559fe4
AtilaSaraiva <atilasaraiva@gmail.com>
2021-11-17 14:45:07 -0300

    reversed change

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def result():
     print(request.form['foo'])
-    return render_template("home.html")
+    return 'Received'
 
 if __name__ == '__main__':
     app.run(debug=True)
